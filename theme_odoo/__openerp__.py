3f32c635966f12e9e71c7faceb415e960dbab8e3
oquintansocampo <oquintansocampo@danielcastelao.org>
2016-01-29 13:44:33 +0100

    1

---
+++ theme_odoo/__openerp__.py
@@ -0,0 +1,9 @@
+{
+  'name':'Tutorial theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Your name',
+  'data': ['views/layout.xml','views/pages.xml'],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_less']
+}
